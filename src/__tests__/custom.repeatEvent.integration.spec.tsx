06478be2cda07e754e24f229d182e01f76877282
이태영 <lhlhgodz3@gmail.com>
2025-08-28 05:07:10 +0900

    🔧 refactor: 주석 처리된 코드 및 불필요한 import 제거로 테스트 파일 정리

--- src/__tests__/custom.repeatEvent.integration.spec.tsx
+++ src/__tests__/custom.repeatEvent.integration.spec.tsx
@@ -1,15 +1,15 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { render, screen, within } from '@testing-library/react';
-import { UserEvent, userEvent } from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import { http, HttpResponse } from 'msw';
 import { SnackbarProvider } from 'notistack';
 import { ReactElement } from 'react';
 
-import { setupMockHandlerCreation } from '../__mocks__/handlersUtils';
+// import { setupMockHandlerCreation } from '../__mocks__/handlersUtils';
 import App from '../App';
 import { server } from '../setupTests';
-import { Event } from '../types';
+// import { Event } from '../types';
 
 const theme = createTheme();
 
@@ -29,26 +29,26 @@ const setup = (element: ReactElement) => {
 };
 
 // 기본 일정 생성 헬퍼 함수
-const saveSchedule = async (
-  user: UserEvent,
-  form: Omit<Event, 'id' | 'notificationTime' | 'repeat'>
-) => {
-  const { title, date, startTime, endTime, location, description, category } = form;
+// const saveSchedule = async (
+//   user: UserEvent,
+//   form: Omit<Event, 'id' | 'notificationTime' | 'repeat'>
+// ) => {
+//   const { title, date, startTime, endTime, location, description, category } = form;
 
-  await user.click(screen.getAllByText('일정 추가')[0]);
+//   await user.click(screen.getAllByText('일정 추가')[0]);
 
-  await user.type(screen.getByLabelText('제목'), title);
-  await user.type(screen.getByLabelText('날짜'), date);
-  await user.type(screen.getByLabelText('시작 시간'), startTime);
-  await user.type(screen.getByLabelText('종료 시간'), endTime);
-  await user.type(screen.getByLabelText('설명'), description);
-  await user.type(screen.getByLabelText('위치'), location);
-  await user.click(screen.getByLabelText('카테고리'));
-  await user.click(within(screen.getByLabelText('카테고리')).getByRole('combobox'));
-  await user.click(screen.getByRole('option', { name: `${category}-option` }));
+//   await user.type(screen.getByLabelText('제목'), title);
+//   await user.type(screen.getByLabelText('날짜'), date);
+//   await user.type(screen.getByLabelText('시작 시간'), startTime);
+//   await user.type(screen.getByLabelText('종료 시간'), endTime);
+//   await user.type(screen.getByLabelText('설명'), description);
+//   await user.type(screen.getByLabelText('위치'), location);
+//   await user.click(screen.getByLabelText('카테고리'));
+//   await user.click(within(screen.getByLabelText('카테고리')).getByRole('combobox'));
+//   await user.click(screen.getByRole('option', { name: `${category}-option` }));
 
-  await user.click(screen.getByTestId('event-submit-button'));
-};
+//   await user.click(screen.getByTestId('event-submit-button'));
+// };
 
 describe('반복 일정 통합 테스트', () => {
   beforeEach(() => {
