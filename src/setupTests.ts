18efbc36b0be7ba34a7032fd4f8fee00f0592e1a
이태영 <lhlhgodz3@innogird.com>
2025-08-28 18:09:47 +0900

    🚑 chore: 전역 테스트 타임아웃을 30초로 증가시켜 테스트 안정성 우선 처리

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,6 +3,9 @@ import '@testing-library/jest-dom';
 
 import { handlers } from './__mocks__/handlers';
 
+// 전역 테스트 타임아웃 설정 (30초) --일단 해결법이 마땅한 부분이 없어서 타임아웃을 30초로 늘리는 방법을 선택.
+vi.setConfig({ testTimeout: 30000 });
+
 // ! Hard 여기 제공 안함
 /* msw */
 export const server = setupServer(...handlers);
